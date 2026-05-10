2e78d4b8ba32c82b8eb7da073bb87ded8d3af323
Antoine Martin <antoine@xpra.org>
2026-05-10 18:01:15 +0700

    because of macos environment shenanigans, we have to use gi_import

--- xpra/client/gtk3/__init__.py
+++ xpra/client/gtk3/__init__.py
@@ -3,6 +3,6 @@
 # Xpra is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-import gi
-gi.require_version("Gtk", "3.0")  # @UndefinedVariable
-gi.require_version("Gdk", "3.0")  # @UndefinedVariable
+from xpra.os_util import gi_import
+gi_import("Gtk")
+gi_import("Gdk")
